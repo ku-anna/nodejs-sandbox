f13a1e32c2b58439b33cf1b580a34e5d7ee81276
Ann K <kubareva.ann@gmail.com>
2025-09-28 19:38:48 +0300

    refresh session

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 import { getEnvVar } from './utils/getEnvVar.js';
 import router from './routers/index.js';
@@ -15,6 +16,7 @@ const PORT = Number(getEnvVar('PORT', '3000'));
 export const startServer = () => {
   const app = express();
 
+  // Парсинг JSON
   app.use(
     express.json({
       type: ['application/json', 'application/vnd.api+json'],
@@ -22,9 +24,11 @@ export const startServer = () => {
     }),
   );
 
-  app.use(express.json());
+  // CORS і cookies
   app.use(cors());
+  app.use(cookieParser());
 
+  // Логування
   app.use(
     pino({
       transport: {
@@ -33,17 +37,23 @@ export const startServer = () => {
     }),
   );
 
+  // Проста перевірка
   app.get('/', (req, res) => {
     res.json({
       message: 'Hello World!',
     });
   });
 
+  // Підключення основного роутера
   app.use(router);
+
+  // Middleware для 404
   app.use(notFoundHandler);
 
+  // Middleware для обробки помилок
   app.use(errorHandler);
 
+  // Запуск сервера
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
