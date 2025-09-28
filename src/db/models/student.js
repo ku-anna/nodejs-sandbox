6b60e8a18b9752413c6d607b377aced053e7cb1b
Ann K <kubareva.ann@gmail.com>
2025-09-28 20:06:23 +0300

    module 5 complete

--- src/db/models/student.js
+++ src/db/models/student.js
@@ -1,6 +1,6 @@
 // src/db/models/student.js
 
-import { Schema, model } from 'mongoose';
+import { model, Schema } from 'mongoose';
 
 const studentsSchema = new Schema(
   {
@@ -26,6 +26,11 @@ const studentsSchema = new Schema(
       required: true,
       default: false,
     },
+    parentId: {
+      // нова властивість
+      type: Schema.Types.ObjectId,
+      ref: 'users',
+    },
   },
   {
     timestamps: true,
@@ -33,14 +38,4 @@ const studentsSchema = new Schema(
   },
 );
 
-// Останнім кроком створюємо модель студента StudentsCollection за допомогою схеми.
 export const StudentsCollection = model('students', studentsSchema);
-
-// Першим аргументом у конструктор схеми ми передаємо об'єкт, де кожний ключ відповідає полю у документі, а їх значеннями є об'єкти, що описують ці поля:
-// type — вказує тип даних, який ми очікуємо для цього поля. У нашому випадку це String, Number та Boolean.
-// required — ця властивість вказує, чи є поле обов'язковим для заповнення. Якщо встановлено значення true, то поле повинно бути відправлене з кожним запитом.
-// enum — це перелік допустимих значень для поля. У нашому випадку для поля gender можливі тільки значення "male", "female" або "other".
-// default — вказує значення за замовчуванням, якщо поле не вказано при створенні документа.
-// Другим аргументом передаємо додаткові параметри схеми:
-// timestamps — встановлює значення true, щоб автоматично створювати поля createdAt та updatedAt, які вказують на час створення та оновлення документа.
-// versionKey — вказує, чи має бути створене поле __v для відстеження версій документу. У нашому випадку ми встановлюємо false, щоб це поле не створювалося.
--- src/routers/students.js
+++ src/routers/students.js
@@ -18,6 +18,10 @@ import {
   updateStudentSchema,
 } from '../validation/students.js';
 
+import { authenticate } from '../middlewares/authenticate.js';
+import { checkRoles } from '../middlewares/checkRoles.js';
+import { ROLES } from '../constants/index.js';
+
 const router = Router();
 
 router.get('/', ctrlWrapper(getStudentsController));
@@ -44,6 +48,48 @@ router.patch(
   isValidId,
   validateBody(updateStudentSchema),
   ctrlWrapper(patchStudentController),
+);
+router.use(authenticate);
+
+router.get('/', ctrlWrapper(getStudentsController));
+
+router.get('/', checkRoles(ROLES.TEACHER), ctrlWrapper(getStudentsController));
+
+router.get(
+  '/:studentId',
+  checkRoles(ROLES.TEACHER, ROLES.PARENT),
+  isValidId,
+  ctrlWrapper(getStudentByIdController),
+);
+
+router.post(
+  '/',
+  checkRoles(ROLES.TEACHER),
+  validateBody(createStudentSchema),
+  ctrlWrapper(createStudentController),
+);
+
+router.put(
+  '/:studentId',
+  checkRoles(ROLES.TEACHER),
+  isValidId,
+  validateBody(createStudentSchema),
+  ctrlWrapper(upsertStudentController),
+);
+
+router.patch(
+  '/:studentId',
+  checkRoles(ROLES.TEACHER, ROLES.PARENT),
+  isValidId,
+  validateBody(updateStudentSchema),
+  ctrlWrapper(patchStudentController),
+);
+
+router.delete(
+  '/:studentId',
+  checkRoles(ROLES.TEACHER),
+  isValidId,
+  ctrlWrapper(deleteStudentController),
 );
 
 export default router;
