c8accb6730c06e1d7d586b2f49fe86f521e8e830
Ann K <kubareva.ann@gmail.com>
2025-09-28 19:07:10 +0300

    module 5: acc and ref tokens

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,6 +1,17 @@
 // src/controllers/auth.js
 
 import { registerUser } from '../services/auth.js';
+import { loginUser } from '../services/auth.js';
+
+export const loginUserController = async (req, res) => {
+  const user = await loginUser(req.body);
+
+  res.status(200).json({
+    status: 200,
+    message: 'Successfully logged in!',
+    data: user,
+  });
+};
 
 export const registerUserController = async (req, res) => {
   const user = await registerUser(req.body);
